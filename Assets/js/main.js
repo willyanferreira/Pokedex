802e27b1fbadd9552fdbba0616d6a54e14e690ee
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-11-22 17:07:43 -0300

    primeiros testes na pokédex
    conseguindo listar um pokémon.
    conseguindo aumentar a div lentamente.

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,1 +1,49 @@
-document.write('Conseguirei em nome de Jeses!');
+//  para consultar -> https://github.com/digitalinnovationone/js-developer-pokedex
+
+const meuh1 = document.querySelector('#teste');
+
+function MyDiv(props) {
+    meuh1.innerHTML = `
+        <li>Nome: ${props.name}</li>
+        <li>Ordem: ${props.order}</li>
+        <li>Experiência: ${props.base_experience}</li>
+        <li>Peso: ${props.weight}kg</li>
+    `
+}
+
+// document.write('Conseguirei em nome de Jeses!');
+
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+let offset = 0;
+const limit = 1;
+
+function exibePokemon(offset) {
+    meuh1.setAttribute('class', 'transicao');
+    const myRequest = fetch(`${baseUrl}?offset=${offset}&limit=${limit}`)
+        .then(response => response.json())
+        .then(data => data.results)
+        .then(results => fetch(results[0].url))
+        .then(resultsResponse => resultsResponse.json())
+        // .then(resultsData => console.log(resultsData))
+        .then(resultsData => resultsData)
+        .then(MyDiv);
+}
+
+exibePokemon(offset);
+
+
+// console.log(myRequest);
+
+document.querySelector('#next').addEventListener('click', () => {
+    offset += 1;
+    exibePokemon(offset)
+})
+
+document.querySelector('#previous').addEventListener('click', () => {
+    if (offset <= 0) {
+        alert('Não existe pokémon anteriro a esse.');
+    } else {
+        offset -= 1;
+        exibePokemon(offset);
+    }
+})
