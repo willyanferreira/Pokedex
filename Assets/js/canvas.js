60a3e03b94346ca5977e24b20283d21287e0cd03
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-11-24 22:51:11 -0300

    Update canvas.js
    Desenho da pokédex finalizado

--- Assets/js/canvas.js
+++ Assets/js/canvas.js
@@ -7,18 +7,92 @@ let myCanvasBottom = document.querySelector('#pokedexBottom');
 {
     let myCanvasContext = myCanvas.getContext('2d');
     // ARCOS DA ESQUERDA
+    // ARCO INTERNO DA ESQUERDA
     myCanvasContext.beginPath();
     myCanvasContext.arc(0, 0, 40, 0, 7);
     myCanvasContext.stroke();
+
+    // ARCO EXTERNO DA ESQUERDA
     myCanvasContext.beginPath();
-    myCanvasContext.arc(0, 0, 60, 0, 7);
+    myCanvasContext.arc(0, 0, 80, 0, 7);
     myCanvasContext.stroke();
 
+    // ARCO CENTRAL BRANCO
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(155, 150, 80, 0, 7);
+    myCanvasContext.stroke();
+    myCanvasContext.fillStyle = '#fff';
+    myCanvasContext.fill();
+
+    // ARCO CENTRAL PRETO
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(155, 150, 70, 0, 7);
+    myCanvasContext.stroke();
+    myCanvasContext.fillStyle = '#000';
+    myCanvasContext.fill();
+
+    // ARCO CENTRAL AZUL
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(155, 150, 45, 0, 7);
+    myCanvasContext.stroke();
+    myCanvasContext.fillStyle = '#87CEFA';
+    myCanvasContext.fill();
+
     // ARCOS DA DIREITA
+    // ARCO INTERNO DA DIREITA
     myCanvasContext.beginPath();
     myCanvasContext.arc(300, 0, 40, 0, 7);
     myCanvasContext.stroke();
+
+    // ARCO EXTERNO DA DIREITA
     myCanvasContext.beginPath();
-    myCanvasContext.arc(300, 0, 60, 0, 7);
+    myCanvasContext.arc(300, 0, 80, 0, 7);
+    myCanvasContext.stroke();
+}
+
+// DESENHANDO A PARTE DE BAIXO DA POKÉDEX
+{
+    let myCanvasContext = myCanvasBottom.getContext('2d');
+    // ARCOS DA ESQUERDA
+    // ARCO INTERNO DA ESQUERDA
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(0, 0, 40, 0, 7);
+    myCanvasContext.stroke();
+
+    // ARCO EXTERNO DA ESQUERDA
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(0, 0, 80, 0, 7);
+    myCanvasContext.stroke();
+
+    // ARCO CENTRAL BRANCO
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(145, 150, 80, 0, 7);
+    myCanvasContext.stroke();
+    myCanvasContext.fillStyle = '#fff';
+    myCanvasContext.fill();
+
+    // ARCO CENTRAL PRETO
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(145, 150, 70, 0, 7);
+    myCanvasContext.stroke();
+    myCanvasContext.fillStyle = '#000';
+    myCanvasContext.fill();
+
+    // ARCO CENTRAL AZUL
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(145, 150, 45, 0, 7);
+    myCanvasContext.stroke();
+    myCanvasContext.fillStyle = '#87CEFA';
+    myCanvasContext.fill();
+
+    // ARCOS DA DIREITA
+    // ARCO INTERNO DA DIREITA
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(300, 0, 40, 0, 7);
+    myCanvasContext.stroke();
+
+    // ARCO EXTERNO DA DIREITA
+    myCanvasContext.beginPath();
+    myCanvasContext.arc(300, 0, 80, 0, 7);
     myCanvasContext.stroke();
 }
